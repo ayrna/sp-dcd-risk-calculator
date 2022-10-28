18f38e3e962547d1b91f1b5d4a04bc835730d1f7
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-10-28 11:24:42 +0200

    Working Switchs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const R = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setCount(count + 16);
+    } else {
+      setCount(count - 16);
+    }
+  };
+
+  const IF = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+    //TODO
+  };
+
+  const M = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>R</h1>
+    <label className="switch">
+      <input onChange={R} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>IF</h1>//TODO
+    <label className="switch">
+      <input onChange={IF} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>F</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>E</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>I</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>M</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>C</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>T</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>S</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>W</h1>
+    <label className="switch">
+      <input onChange={M} type="checkbox"/>
+      <span className="slider"></span>
+    </label>
+    <h1>{count}</h1>
     </div>
   );
 }
-
-export default App;
